a8fb27848600609a1513f0659ca6e3bfe1052704
Mayank Sahu <mayank.sahu@joshsoftware.com>
2021-10-21 17:07:02 +0530

    [amp-access-fewcents] Created static design for the paywall and rendering it

--- extensions/amp-access-fewcents/0.1/fewcents-impl.js
+++ extensions/amp-access-fewcents/0.1/fewcents-impl.js
@@ -14,4 +14,165 @@
  * limitations under the License.
  */
 
-export class AmpAccessFewcents {}
+import {installStylesForDoc} from '../../../src/style-installer';
+import {removeChildren} from '#core/dom';
+import {user} from '../../../src/log';
+
+import {Services} from '#service';
+
+import {CSS} from '../../../build/amp-access-fewcents-0.1.css';
+
+const TAG = 'amp-access-fewcents';
+
+/**
+ * @implements {../../amp-access/0.1/access-vendor.AccessVendor}
+ */
+export class AmpAccessFewcents {
+  /**
+   * @param {!../../amp-access/0.1/amp-access.AccessService} accessService
+   */
+  constructor(accessService) {
+    /** @const */
+    this.ampdoc = accessService.ampdoc;
+
+    /** @private {boolean} */
+    this.containerEmpty_ = true;
+
+    /** @private {?Node} */
+    this.innerContainer_ = null;
+
+    /** @const @private {!../../../src/service/vsync-impl.Vsync} */
+    this.vsync_ = Services.vsyncFor(this.ampdoc.win);
+
+    // Install styles.
+    installStylesForDoc(this.ampdoc, CSS, () => {}, false, TAG);
+  }
+
+  /**
+   * @return {!Promise<!JsonObject>}
+   */
+  authorize() {
+    this.emptyContainer_().then(this.renderPurchaseOverlay_.bind(this));
+    return {access: false};
+  }
+
+  /**
+   * @param {string} name
+   * @return {!Element}
+   * @private
+   */
+  createElement_(name) {
+    return this.ampdoc.win.document.createElement(name);
+  }
+
+  /**
+   * @return {!Element} element where paywall will be displayed
+   * @private
+   */
+  getPaywallContainer_() {
+    const id = TAG + '-dialog';
+    const dialogContainer = this.ampdoc.getElementById(id);
+    return user().assertElement(
+      dialogContainer,
+      'No element found with id ' + id
+    );
+  }
+
+  /**
+   * @private
+   * @return {!Promise}
+   */
+  emptyContainer_() {
+    if (this.containerEmpty_) {
+      return Promise.resolve();
+    }
+
+    return this.vsync_.mutatePromise(() => {
+      this.containerEmpty_ = true;
+      this.innerContainer_ = null;
+      removeChildren(this.getPaywallContainer_());
+    });
+  }
+
+  /**
+   * @private
+   */
+  renderPurchaseOverlay_() {
+    const dialogContainer = this.getPaywallContainer_();
+
+    this.innerContainer_ = this.createElement_('div');
+    this.innerContainer_.className = TAG + '-container';
+
+    this.innerContainer_.appendChild(
+      this.createAndAddProperty_(
+        'header',
+        'Instant Access With Fewcents.',
+        '-header'
+      )
+    );
+
+    this.innerContainer_.appendChild(
+      this.createAndAddProperty_('header', 'INR 50.46', '-article-price')
+    );
+
+    this.innerContainer_.appendChild(
+      this.createAndAddProperty_(
+        'p',
+        'First 1 unlocks are free!',
+        '-no-of-Unlock'
+      )
+    );
+
+    this.innerContainer_.appendChild(
+      this.createAndAddProperty_('button', 'Unlock', '-purchase-button')
+    );
+
+    this.innerContainer_.appendChild(this.createRefRowElement_());
+
+    dialogContainer.appendChild(this.innerContainer_);
+    this.containerEmpty_ = false;
+  }
+
+  /**
+   * @private
+   */
+  createRefRowElement_() {
+    const refRow = this.createElement_('div');
+    refRow.className = TAG + '-refRow';
+    refRow.appendChild(
+      this.createAnchorElement_('Terms', 'https://www.fewcents.co/terms')
+    );
+
+    refRow.appendChild(
+      this.createAnchorElement_('Privacy', 'https://www.fewcents.co/privacy')
+    );
+    refRow.appendChild(
+      this.createAnchorElement_('Contact Us', 'mailto:support@fewcents.co')
+    );
+
+    return refRow;
+  }
+
+  /**
+   * @private
+   */
+  createAndAddProperty_(elementType, text, className) {
+    const element = this.createElement_(elementType);
+    element.className = TAG + className;
+    element.textContent = text;
+    return element;
+  }
+
+  /**
+   * @private
+   */
+  createAnchorElement_(text, href) {
+    const element = this.createElement_('a');
+    element.className = TAG + '-refElements';
+    element.href = href;
+    element.target = '_blank';
+    element.textContent = text;
+    element.rel = 'noopener noreferrer';
+    return element;
+  }
+}
