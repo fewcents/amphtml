de26f82b12c654c226416127a3f70a418790d201
Mayank Sahu <mayank.sahu@joshsoftware.com>
2021-11-22 17:41:25 +0530

    [amp-access-fewcents] Added testcases for the authorize function

--- extensions/amp-access-fewcents/0.1/test/test-amp-access-fewcents.js
+++ extensions/amp-access-fewcents/0.1/test/test-amp-access-fewcents.js
@@ -14,11 +14,32 @@
  * limitations under the License.
  */
 
-import '../amp-access-fewcents';
-import {htmlFor} from '#core/dom/static-template';
+import {AmpAccessFewcents} from '../fewcents-impl';
+
+const paywallResponse = {
+  success: true,
+  message: 'Amp article price returned with unlock url.',
+  data: {
+    loginUrl:
+      'https://wallet.hounds.fewcents.co?login_source=amp_paywall&login_publisher_id=47&login_publisher_bid_id=90531&bid_mode=paywall&bid_price=622.38&login_redirect_url=http%3A%2F%2Flocalhost%3A8000%2Fexamples%2Famp-access-fewcents.html&login_publisher_logo_url=https%3A%2F%2Fi1.wp.com%2Fthefix.media%2Fwp-content%2Fuploads%2F2021%2F04%2Flogo-no-margins.png&amp_reader_id=amp-yfpD7ewsxdSdbH4xCboNIw',
+    purchaseOptions: [
+      {
+        price: {
+          currency: 'INR',
+          price: 622.38,
+        },
+        unlockUrl:
+          'https://wallet.hounds.fewcents.co?login_source=amp_paywall&login_publisher_id=47&login_publisher_bid_id=90531&bid_mode=paywall&bid_price=622.38&login_redirect_url=http%3A%2F%2Flocalhost%3A8000%2Fexamples%2Famp-access-fewcents.html&login_publisher_logo_url=https%3A%2F%2Fi1.wp.com%2Fthefix.media%2Fwp-content%2Fuploads%2F2021%2F04%2Flogo-no-margins.png&amp_reader_id=amp-yfpD7ewsxdSdbH4xCboNIw',
+      },
+    ],
+    bidId: 90531,
+    access: false,
+  },
+  statusCode: 402,
+};
 
 describes.realWin(
-  'amp-access-fewcents-v0.1',
+  'amp-access-fewpaywallResponsecents-v0.1',
   {
     amp: {
       runtimeOn: true,
@@ -26,24 +47,129 @@ describes.realWin(
     },
   },
   (env) => {
-    let win;
-    let doc;
-    let html;
+    // let win;
+    // let doc;
+    // let html;
+    // let document,
+    let ampdoc;
+    let accessSource;
+    let accessService;
+    let accessSourceMock;
+    let xhrMock;
+    let fewcentsConfig;
+    let vendor;
 
     beforeEach(() => {
-      win = env.win;
-      doc = win.document;
-      html = htmlFor(doc);
+      // win = env.win;
+      ampdoc = env.ampdoc;
+      // document = win.document;
+
+      fewcentsConfig = {
+        articleTitleSelector: '#laterpay-test-title',
+        region: 'us',
+      };
+
+      accessSource = {
+        getAdapterConfig: () => {
+          return fewcentsConfig;
+        },
+        buildUrl: () => {},
+        loginWithUrl: () => {},
+        getLoginUrl: () => {},
+      };
+
+      accessService = {
+        ampdoc,
+        getSource: () => accessSource,
+      };
+
+      accessSourceMock = env.sandbox.mock(accessSource);
+      vendor = new AmpAccessFewcents(accessService, accessSource);
+      xhrMock = env.sandbox.mock(vendor.xhr_);
     });
 
-    it('should contain "hello world" when built', async () => {
-      const element = html`
-        <amp-access-fewcents width="100" height="10" layout="responsive">
-        </amp-access-fewcents>
-      `;
-      doc.body.appendChild(element);
-      await element.whenBuilt();
-      expect(element.querySelector('div').textContent).to.equal('hello world');
+    afterEach(() => {
+      accessSourceMock.verify();
+      xhrMock.verify();
+    });
+
+    describe('authorize', () => {
+      let emptyContainerStub;
+      beforeEach(() => {
+        emptyContainerStub = env.sandbox.stub(vendor, 'emptyContainer_');
+        env.sandbox.stub(vendor, 'renderPurchaseOverlay_');
+      });
+
+      it('should show the premium content', () => {
+        accessSourceMock
+          .expects('buildUrl')
+          .returns(Promise.resolve(''))
+          .once();
+
+        xhrMock
+          .expects('fetchJson')
+          .returns(
+            Promise.resolve({
+              json() {
+                return Promise.resolve({access: true});
+              },
+            })
+          )
+          .once();
+
+        return vendor.authorize().then((resp) => {
+          expect(resp.access).to.be.true;
+          expect(emptyContainerStub.called).to.be.true;
+        });
+      });
+
+      it('should show the paywall : authorization response fails - 402 error', () => {
+        accessSourceMock
+          .expects('buildUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+
+        xhrMock
+          .expects('fetchJson')
+          .returns(
+            Promise.reject({
+              response: {
+                status: 402,
+                json() {
+                  return Promise.resolve(paywallResponse);
+                },
+              },
+            })
+          )
+          .once();
+
+        emptyContainerStub.returns(Promise.resolve());
+        return vendor.authorize().then((res) => {
+          expect(res.access).to.be.false;
+        });
+      });
+
+      it('should show the premium : authorization response fails - 500 error', () => {
+        accessSourceMock
+          .expects('buildUrl')
+          .returns(Promise.resolve('https://builturl'))
+          .once();
+
+        xhrMock
+          .expects('fetchJson')
+          .returns(
+            Promise.reject({
+              response: {
+                status: 500,
+              },
+            })
+          )
+          .once();
+
+        return vendor.authorize().then((res) => {
+          expect(res.access).to.be.true;
+        });
+      });
     });
   }
 );
